1a97e51cbc0198542d1c46538a0b2b9352c1a5fb
kdy1242 <dy12311229@gmail.com>
2021-11-08 23:24:48 +0900

    countDownTimer

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,11 +12,25 @@ class MainActivity : AppCompatActivity() {
 
         val lotteryNumbers = arrayListOf(number1, number2, number3, number4, number5, number6)
 
+        val countDownTimer = object : CountDownTimer(3000, 100) {
+            override fun onTick(millisUntilFinished: Long) {
+                lotteryNumbers.forEach {
+                    val randomNumber = (Math.random() * 45 + 1).toInt()
+                    it.text = "${randomNumber}"
+                }
+            }
+
+            override fun onFinish() {
+            }
+        }
+        
         lotteryButton.setOnClickListener {
-            lotteryButton.playAnimation()
-            lotteryNumbers.forEach {
-                val randomNumber = (Math.random() * 45 + 1).toInt()
-                it.text = "${randomNumber}"
+            if (lotteryButton.isAnimating) {
+                lotteryButton.cancelAnimation()
+                countDownTimer.cancel()
+            } else {
+                lotteryButton.playAnimation()
+                countDownTimer.start()
             }
         }
     }
