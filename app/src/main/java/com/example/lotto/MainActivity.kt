74c2156604d0b86039958a29a923134ae7cc1191
kdy1242 <dy12311229@gmail.com>
2021-11-08 23:13:20 +0900

    로또번호 랜덤생성하기

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lotteryNumbers = arrayListOf(number1, number2, number3, number4, number5, number6)
+
+        lotteryButton.setOnClickListener {
+            lotteryButton.playAnimation()
+            lotteryNumbers.forEach {
+                val randomNumber = (Math.random() * 45 + 1).toInt()
+                it.text = "${randomNumber}"
+            }
+        }
     }
 }
